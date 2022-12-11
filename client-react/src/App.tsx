fdb14b91ac58b0e223216faaafae5ce929e69f52
Minh Thaor <minhthao5648@gmail.com>
2022-12-11 11:59:17 +0700

    done config envoy

--- client-react/src/App.tsx
+++ client-react/src/App.tsx
@@ -6,6 +6,7 @@ function App() {
   async function fetchData() {
     const resp = await addTodo();
     console.log({ resp });
+    console.log({ description: resp.getDescription() });
   }
 
   return (
